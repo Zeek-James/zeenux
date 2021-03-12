96b83d636400ef115c1af78b16025a23b72b5d62
Isaac James <chizuruokej@yahoo.com>
2021-03-12 21:29:15 +0100

    new commit

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+// app.get('/', (req, res) => { res.send('Hello from Express!')
 
 app.use("/api/transactions", transactions);
 
